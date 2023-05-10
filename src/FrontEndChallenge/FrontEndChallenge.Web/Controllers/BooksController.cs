b46aaea6e25b6b54a8103f2fe79d353b67ddc8c6
Mark Norman <mark.norman@ukho.gov.uk>
2023-05-10 12:58:52 +0100

    first working

--- src/FrontEndChallenge/FrontEndChallenge.Web/Controllers/BooksController.cs
+++ src/FrontEndChallenge/FrontEndChallenge.Web/Controllers/BooksController.cs
@@ -8,23 +8,19 @@ public class BooksController : ControllerBase
     private List<Book> books = new List<Book>()
         {
              new Book { Id = 1, Title = "Something", Author = "Someone", CoverImage = "https://picsum.photos/200/300" },
-
              new Book { Id = 2, Title = "How to test", Author = "Mr Tester", CoverImage = "https://picsum.photos/200/300" },
-
              new Book { Id = 3, Title = "Writing a book", Author = "Author", CoverImage = "https://picsum.photos/200/300" }
 
         };
 
     [HttpGet]
     public ActionResult GetBooks()
-
     {
         return Ok(books);
     }
 
-    [HttpGet]
-    public ActionResult GetBooks(string searchTerm)
-
+    [HttpGet("{id}")]
+    public ActionResult SearchBooks(string searchTerm)
     {
         var filteredBooks = books.Where(book => book.Title.ToLower().Contains(searchTerm.ToLower())).ToList();
 
