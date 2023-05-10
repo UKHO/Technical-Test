13dbe9c65d4247d41352f3a1f7b9187b48d62b34
alok-singh-ukho <118981402+alok-singh-ukho@users.noreply.github.com>
2023-05-10 10:21:09 +0100

    frontEndChallenge first go

---
+++ src/FrontEndChallenge/FrontEndChallenge.Web/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+
+{
+    private List<Book> books = new List<Book>()
+        {
+             new Book { Id = 1, Title = "Something", Author = "Someone", CoverImage = "https://picsum.photos/200/300" },
+
+             new Book { Id = 2, Title = "How to test", Author = "Mr Tester", CoverImage = "https://picsum.photos/200/300" },
+
+             new Book { Id = 3, Title = "Writing a book", Author = "Author", CoverImage = "https://picsum.photos/200/300" }
+
+        };
+
+    [HttpGet]
+    public ActionResult GetBooks()
+
+    {
+        return Ok(books);
+    }
+
+    [HttpGet]
+    public ActionResult GetBooks(string searchTerm)
+
+    {
+        var filteredBooks = books.Where(book => book.Title.ToLower().Contains(searchTerm.ToLower())).ToList();
+
+        return Ok(filteredBooks);
+    }
+}
---
+++ src/FrontEndChallenge/FrontEndChallenge.Web/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿public class Book
+
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public string CoverImage { get; set; }
+}
